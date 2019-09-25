b5809f2894ec7cc71ad2ec5c8b069807f96f44fb
crisri@yandex.com <wb.zhouquan@cn.net.ntes>
2019-09-25 18:47:49 +0800

    默认项目配置2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,24 @@
 module.exports = {
     title: 'crisri\'s blog' ,
-    description: 'Just playing around'
+    description: 'Just playing around',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+        {
+          text: 'Languages',
+          ariaLabel: 'Language Menu',
+          items: [
+            { text: 'Chinese', link: '/language/chinese/' },
+            { text: 'Japanese', link: '/language/japanese/' }
+          ]
+        }
+      ],
+      sidebar: [
+        '/',
+        '/page-a',
+        ['/page-b', 'Explicit link text']
+      ]
+    }
   }
